3a3590080388d6a2909c5fbdb3033f398268a038
Elvis-rugamba <elvisrugamba@gmail.com>
2019-11-19 07:25:54 +0100

    Finish get a specific red-flag records [Delivers #169802329]

--- api/controllers/red-flags.js
+++ api/controllers/red-flags.js
@@ -16,3 +16,32 @@ exports.redFlags_get_all = async (req, res, next) => {
     });
   }
 };
+
+exports.redFlags_get_redFlag = async (req, res, next) => {
+  const id = req.params.redFlagId;
+  const redFlag = await RedFlag.findById(id);
+  if (!redFlag) {
+    return res.status(400).json({
+      status: 400,
+      error: 'The red-flag with the given ID not found',
+    });
+  }
+  if (req.userData.userType === 'user') {
+    if (redFlag.createdBy !== req.userData.userId) {
+      return res.status(403).json({
+        status: 403,
+        error: 'Access denied',
+      });
+    }
+    return res.status(200).json({
+      status: 200,
+      data: redFlag,
+    });
+  }
+  if (req.userData.userType === 'admin') {
+    return res.status(200).json({
+      status: 200,
+      data: redFlag,
+    });
+  }
+};
