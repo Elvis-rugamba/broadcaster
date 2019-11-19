17cd0cbe02a5a5d61cad80816efde23565be8652
Elvis-rugamba <elvisrugamba@gmail.com>
2019-11-19 07:49:31 +0100

    Finish create a red-flag record [Delivers #169802331]

--- api/routes/red-flags.js
+++ api/routes/red-flags.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const checkAuth = require('../middlewares/check-auth');
+const upload = require('../middlewares/upload');
 const RedFlagsController = require('../controllers/red-flags');
 
 const router = express.Router();
@@ -7,5 +8,7 @@ const router = express.Router();
 router.get('/', checkAuth, RedFlagsController.redFlags_get_all);
 
 router.get('/:redFlagId', checkAuth, RedFlagsController.redFlags_get_redFlag);
+
+router.post('/', checkAuth, upload.fields([{ name: 'images', maxCount: 5 }, { name: 'videos', maxCount: 5 }]), RedFlagsController.redFlags_create_redFlag);
 
 module.exports = router;
