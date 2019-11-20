6072814f54c0e50fabc55ccc69978c38c9688e22
Elvis-rugamba <elvisrugamba@gmail.com>
2019-11-20 02:44:28 +0100

    Finish host app to heroku [Delivers #169834771]

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const swaggerOptions = {
 };
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
