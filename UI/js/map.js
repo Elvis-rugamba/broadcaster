83c8b4aa46d0ca483f2ce02be7042bb1e15415e6
Elvis-rugamba <elvisrugamba@gmail.com>
2019-12-03 23:50:14 +0100

    Change locationMap function to arrow function

--- UI/js/map.js
+++ UI/js/map.js
@@ -1,44 +1,44 @@
-//Location Map
-function locationMap() {
-    // The location
-    const center = {lat:  -1.8964, lng: 30.0569};
+// Location Map
+const locationMap = () => {
+  // The location
+  const center = { lat: -1.8964, lng: 30.0569 };
 
-    const mapProp= {
-        panControl: true,
-        zoomControl: true,
-        zoomControlOptions: {
-            style: google.maps.ZoomControlStyle.DEFAULT
-        },
-        mapTypeControl: true,
-        mapTypeControlOptions: {
-            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-            position: google.maps.ControlPosition.TOP_CENTER
-        },
-        scaleControl: true,
-        streetViewControl: true,
-        overviewMapControl: true,
-        rotateControl: true,
-        center: center,
-        zoom: 14
-    };
+  const mapProp = {
+    panControl: true,
+    zoomControl: true,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.DEFAULT,
+    },
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+      position: google.maps.ControlPosition.TOP_CENTER,
+    },
+    scaleControl: true,
+    streetViewControl: true,
+    overviewMapControl: true,
+    rotateControl: true,
+    center: center,
+    zoom: 14,
+  };
     // The map, centered at Location
-    const map = new google.maps.Map(
-        document.querySelector('.location-map'), mapProp);
+  const map = new google.maps.Map(
+    document.querySelector('.location-map'), mapProp,
+  );
 
-    // The marker, positioned at Location
-    const marker = new google.maps.Marker({
-        position: center,
-        map: map,
-        animation: google.maps.Animation.DROP
-    });
+  // The marker, positioned at Location
+  const marker = new google.maps.Marker({
+    position: center,
+    map: map,
+    animation: google.maps.Animation.DROP,
+  });
 
-    const infoWindow = new google.maps.InfoWindow({
-        content: 'Latitude: ' + map.lat() +
-        '<br>Longitude: ' + map.lng()
-    });
+  const infoWindow = new google.maps.InfoWindow({
+    content: `Latitude: ${map.lat()}<br>Longitude: ${map.lng()}`,
+  });
 
-    google.maps.event.addListerner(marker, 'mouseover', function () {
-        infoWindow.open(map, marker);
-    });
-}
+  google.maps.event.addListerner(marker, 'mouseover', () => {
+    infoWindow.open(map, marker);
+  });
+};
 // End Location Map
