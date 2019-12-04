78e4d497a04e0028678c0941e17f84ea3ea038a2
Elvis-rugamba <elvisrugamba@gmail.com>
2019-12-04 13:48:44 +0100

    Test Db connection

--- server/v2/test/dbConnection.spec.js
+++ server/v2/test/dbConnection.spec.js
@@ -1,14 +1,26 @@
 import dotenv from 'dotenv';
 import chai from 'chai';
 import db from '../db/config';
+import query from '../db/queries';
 
 dotenv.config();
 const { expect } = chai;
-describe('Test Database', () => {
+describe('Test Database', async () => {
+  before(
+    await db.query(query.createTestUsersTable),
+    await db.query(query.createUser),
+  );
+  after(
+    await db.query(query.dropTesUsersTable),
+  );
   it('It should return user data from testusers table', async () => {
-    const dbTest = await db.queryTestConn();
-    expect(process.env.NODE_ENV).to.equal('testing');
-    expect(dbTest).to.be.an('object');
-    expect(dbTest).to.have.property('id');
+    try {
+      const dbTest = await db.queryTestConn();
+      expect(process.env.NODE_ENV).to.equal('testing');
+      expect(dbTest).to.be.an('object');
+      expect(dbTest).to.have.property('id');
+    } catch (error) {
+      expect(() => { throw error; }).to.throw();
+    }
   });
 });
