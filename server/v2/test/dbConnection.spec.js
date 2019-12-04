a0a19cd8c8f8b23ec3391d047a69729b63890044
Elvis-rugamba <elvisrugamba@gmail.com>
2019-12-04 12:26:44 +0100

    Add postgres service to travis

--- server/v2/test/dbConnection.spec.js
+++ server/v2/test/dbConnection.spec.js
@@ -3,7 +3,6 @@ import chai from 'chai';
 import db from '../db/config';
 
 dotenv.config();
-// console.log(db.queryTestConn());
 const { expect } = chai;
 describe('Test Database', () => {
   it('It should return user data from testusers table', async () => {
